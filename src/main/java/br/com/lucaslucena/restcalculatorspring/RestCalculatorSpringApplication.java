99fe7bb151b66b124279764221b0d72c71c87082
Lucas Lucena <lucas.lucenak@gmail.com>
2022-10-11 12:52:15 -0300

    Add Spring dependencies

---
+++ src/main/java/br/com/lucaslucena/restcalculatorspring/RestCalculatorSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucaslucena.restcalculatorspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCalculatorSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCalculatorSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucaslucena/restcalculatorspring/RestCalculatorSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucaslucena.restcalculatorspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCalculatorSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
