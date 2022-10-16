e56f17fcb9dc7f81141beb1ce943dbc77ac77918
Lucas Lucena <lucas.lucenak@gmail.com>
2022-10-16 17:12:41 -0300

    Add power, square, mult, div and sub op

--- src/main/java/br/com/lucaslucena/restcalculatorspring/controllers/MathController.java
+++ src/main/java/br/com/lucaslucena/restcalculatorspring/controllers/MathController.java
@@ -10,16 +10,44 @@ public class MathController {
 
     NumberUtil numberUtil = new NumberUtil();
 
-    @GetMapping("/add/{n1}/{n2}")
+    @GetMapping("/addition/{n1}/{n2}")
     public Double sumTwoNumbers (@PathVariable("n1") String n1, @PathVariable("n2") String n2) {
         Double parsedN1 = numberUtil.parseDouble(n1);
         Double parsedN2 = numberUtil.parseDouble(n2);
         return parsedN1 + parsedN2;
     }
 
-    @GetMapping("/test")
-    public String test() {
-        return "oii";
+    @GetMapping("/subtraction/{n1}/{n2}")
+    public Double subtractTwoNumbers(@PathVariable("n1") String n1, @PathVariable("n2") String n2) {
+        Double parsedN1 = numberUtil.parseDouble(n1);
+        Double parsedN2 = numberUtil.parseDouble(n2);
+        return parsedN1 - parsedN2;
     }
 
+    @GetMapping("/multiplication/{n1}/{n2}")
+    public Double multiplicateTwoNumbers(@PathVariable("n1") String n1, @PathVariable("n2") String n2) {
+        Double parsedN1 = numberUtil.parseDouble(n1);
+        Double parsedN2 = numberUtil.parseDouble(n2);
+        return parsedN1 * parsedN2;
+    }
+
+    @GetMapping("/division/{n1}/{n2}")
+    public Double divideTwoNumbers(@PathVariable("n1") String n1, @PathVariable("n2") String n2) {
+        Double parsedN1 = numberUtil.parseDouble(n1);
+        Double parsedN2 = numberUtil.parseDouble(n2);
+        return parsedN1 / parsedN2;
+    }
+
+    @GetMapping("/square/{n1}")
+    public Double square(@PathVariable("n1") String n1) {
+        Double parsedN1 = numberUtil.parseDouble(n1);
+        return Math.sqrt(parsedN1);
+    }
+
+    @GetMapping("/power/{n1}/{n2}")
+    public Double power(@PathVariable("n1") String n1, @PathVariable("n2") String n2) {
+        Double parsedN1 = numberUtil.parseDouble(n1);
+        Double parsedN2 = numberUtil.parseDouble(n2);
+        return Math.pow(parsedN1, parsedN2);
+    }
 }
