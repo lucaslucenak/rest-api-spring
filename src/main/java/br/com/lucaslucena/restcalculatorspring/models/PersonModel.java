54db257d115d8f49347ab645a8a33296eb8f2073
Lucas Lucena <lucas.lucenak@gmail.com>
2022-10-22 01:39:24 -0300

    Add Postgresql database

--- src/main/java/br/com/lucaslucena/restcalculatorspring/models/PersonModel.java
+++ src/main/java/br/com/lucaslucena/restcalculatorspring/models/PersonModel.java
@@ -1,5 +1,6 @@
 package br.com.lucaslucena.restcalculatorspring.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,14 +11,24 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class PersonModel implements Serializable {
+@Entity
+@Table(name = "tb_person")
+public class PersonModel  {
 
-    @Serial
-    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(nullable = false)
     private String address;
+
+    @Column(nullable = false)
     private String gender;
 }
