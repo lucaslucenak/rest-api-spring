e473cbdc878a3910ab1ca84e3a4db66bd2aff125
Lucas Lucena <lucas.lucenak@gmail.com>
2022-11-04 13:05:28 -0300

    Add JSON Serialization

--- src/main/java/br/com/lucaslucena/restcalculatorspring/data/vo/v2/PersonModelVO2.java
+++ src/main/java/br/com/lucaslucena/restcalculatorspring/data/vo/v2/PersonModelVO2.java
@@ -1,6 +1,9 @@
 package br.com.lucaslucena.restcalculatorspring.data.vo.v2;
 
 import br.com.lucaslucena.restcalculatorspring.models.PersonModel;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,25 +13,22 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonPropertyOrder({"id", "firstName", "lastName", "address", "birthDate", "gender"})
 public class PersonModelVO2 {
-
-    public PersonModelVO2(PersonModel personModel) {
-        id = personModel.getId();
-        firstName = personModel.getFirstName();
-        lastName = personModel.getLastName();
-        address = personModel.getAddress();
-        gender = personModel.getGender();
-    }
 
     private Long id;
 
+    @JsonProperty("first_name")
     private String firstName;
 
+    @JsonProperty("last_name")
     private String lastName;
 
     private String address;
 
+    @JsonIgnore
     private String gender;
 
+    @JsonProperty("birth_date")
     private Date birthDate;
 }
