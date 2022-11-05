807bd9bb5e5bf9bfa9ad4ef197e2a0275df8424c
Lucas Lucena <lucas.lucenak@gmail.com>
2022-11-05 01:25:17 -0300

    Add DozenMapper tests

--- src/test/java/br/com/lucaslucena/restcalculatorspring/unitTests/mappers/DozerMapperTest.java
+++ src/test/java/br/com/lucaslucena/restcalculatorspring/unitTests/mappers/DozerMapperTest.java
@@ -1,6 +1,7 @@
 package br.com.lucaslucena.restcalculatorspring.unitTests.mappers;
 
 import br.com.lucaslucena.restcalculatorspring.data.vo.v1.PersonModelVO;
+import br.com.lucaslucena.restcalculatorspring.data.vo.v2.PersonModelVO2;
 import br.com.lucaslucena.restcalculatorspring.mappers.DozerMapper;
 import br.com.lucaslucena.restcalculatorspring.models.PersonModel;
 import org.aspectj.lang.annotation.Before;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import javax.swing.text.DateFormatter;
 import java.text.DateFormat;
 import java.util.Date;
 
@@ -30,5 +32,14 @@ public class DozerMapperTest {
         Mockito.when(mapper.parseObject(personModelVO, PersonModel.class)).thenReturn(personModel);
 
         Assertions.assertEquals(mapper.parseObject(personModelVO, PersonModel.class), personModel);
+    }
+
+    @Test
+    public void shouldMapPersonVO2IntoPersonModel() {
+        PersonModelVO2 personModelVO2 = new PersonModelVO2(1L, "Lucas", "Lucena", "IREMAR", "M", new Date());
+        PersonModel personModel = new PersonModel(1L, "Lucas", "Lucena", "IREMAR", "M", new Date());
+        Mockito.when(mapper.parseObject(personModelVO2, PersonModel.class)).thenReturn(personModel);
+
+        Assertions.assertEquals(mapper.parseObject(personModelVO2, PersonModel.class), personModel);
     }
 }
